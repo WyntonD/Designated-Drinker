5a23886b3e8e8907573d3bac47bdc1f0c882b831
bhelms2132 <bhelms2132@gmail.com>
2019-09-04 19:39:01 -0400

    database var added

--- assets/javascript/dd.js
+++ assets/javascript/dd.js
@@ -1,1 +1,18 @@
-var apiKey= "AIzaSyC3FQD-MCWfgEGbZZ_FCL1RpnJKgRRiGlg";
+
+
+var Config = {
+    apiKey: "AIzaSyC3FQD-MCWfgEGbZZ_FCL1RpnJKgRRiGlg",
+    authDomain: "designateddrinker-e35a3.firebaseapp.com",
+    databaseURL: "https://designateddrinker-e35a3.firebaseio.com",
+    projectId: "designateddrinker-e35a3",
+    storageBucket: "",
+    messagingSenderId: "545274280980",
+    appId: "1:545274280980:web:93778394c39fcbf1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(Config);
+  // Reference database service
+  var database = firebase.database();
+  
+
+
