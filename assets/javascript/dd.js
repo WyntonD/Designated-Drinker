cfd369f2d8f045e13754da3cee80eb0980c495ed
WD$ <wyntonmdavis@gmail.com>
2019-09-04 18:49:30 -0400

    console logging buttons in javascript

---
+++ assets/javascript/dd.js
@@ -0,0 +1,16 @@
+ $("#btnBeer").on("click", function(){
+    console.log("Beer button clicked")
+
+ });
+
+ $("#btnWine").on("click", function(){
+    console.log("Wine button clicked")
+
+ });
+
+ $("#btnCocktail").on("click", function(){
+    console.log("Cocktail button clicked")
+
+ });
+
+ 
