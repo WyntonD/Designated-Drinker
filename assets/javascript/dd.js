43af9110d4ec7af72e7e0436c1dcd57abc3cb9f4
Sue Look <suelook1@gmail.com>
2019-09-04 21:19:58 -0400

    added vars to js

--- assets/javascript/dd.js
+++ assets/javascript/dd.js
@@ -1,17 +1,24 @@
+//Brian keys
+
 var apiKey= "AIzaSyC3FQD-MCWfgEGbZZ_FCL1RpnJKgRRiGlg";
 
-// grab user input/ user types in a submit button with the name of a cocktail
+// grab user input/ user types in a submit button with the name of a cocktail-Wynton
 
-//create local "temporary" object for holding drink information
+//create local "temporary" object for holding drink information-Sue
+var drinkName = {
+    recipe: drinkRecipe,
+}
 
-//upload the drink data to the database
+//upload the drink data to the database-Willaim
 
-//clear the submit button
+//clear the submit button-Wynton
 
-//create the firebase event for adding the recipe to the database
+//create the firebase event for adding the recipe to the database-Brian
 
-//store everything into a variable
+//store everything into a variable-Sue
+var drinkrecipe = childSnapshot.val().name
 
-//create a new row to add recipe
+//create a new row to add recipe-Willaim
 
-// append the new row to the table
+// append the new row to the table-Sue
+$("#search-table > tbody").append (newRow);
