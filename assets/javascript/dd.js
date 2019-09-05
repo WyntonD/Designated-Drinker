532d67a41189d8bd3acc1e84eb8d055d19b473db
Sue Look <suelook1@gmail.com>
2019-09-04 21:10:46 -0400

    added vars to js

--- assets/javascript/dd.js
+++ assets/javascript/dd.js
@@ -1,1 +1,17 @@
 var apiKey= "AIzaSyC3FQD-MCWfgEGbZZ_FCL1RpnJKgRRiGlg";
+
+// grab user input/ user types in a submit button with the name of a cocktail
+
+//create local "temporary" object for holding drink information
+
+//upload the drink data to the database
+
+//clear the submit button
+
+//create the firebase event for adding the recipe to the database
+
+//store everything into a variable
+
+//create a new row to add recipe
+
+// append the new row to the table
