60eb90c945ccb5d5d9a7e4530b6592ddf36ef97f
Will Woodall <willwoodall@gmail.com>
2019-09-04 20:58:04 -0400

    javascript for uploading to database

--- assets/javascript/dd.js
+++ assets/javascript/dd.js
@@ -1,1 +1,11 @@
 var apiKey= "AIzaSyC3FQD-MCWfgEGbZZ_FCL1RpnJKgRRiGlg";
+
+
+
+
+database.ref().push(drinkName);
+
+var newRow = $("<tr>").append(
+    $("<td>").text(drinkName),
+    $("<td>").text(Ingredients),
+    );
